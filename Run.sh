feb4c54f3930d7782d4fbff54d999d4113d91c6e
Michael Kattner <MDKattner@gmail.com>
2026-01-20 21:47:25 -0600

    Tidy up and qol (#4)
    * Config fixes and spellcheck
    
    * Updated gitignore, packages, and docs
    
    * Updated scripts for improved control flow and reliablity

--- Run.sh
+++ Run.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+set -euo pipefail # Putting the script in 'strict mode'
 
 if ! command -v pipenv &>/dev/null ; then
     echo "This utillity depends on pipenv, please install it in the enviornment this script is being run on" >&2
@@ -20,13 +21,13 @@ function ScriptSelect
 {
     # All scripts that end in '.py' are excluded from the selection menu
     # The preview window only shows lines that begin with '#' excluding those that begin with '#!'
-    pipenv run ls ./scripts/ | grep -Gv "[\_\.]py" | fzf --header="Select What You Want to Do" --header-border=bold --header-label-pos=top --no-multi --preview='pyfiglet -w$FZF_PREVIEW_COLUMNS -fansi_regular -jcenter MatStat && grep -G ^#[^!] ./scripts/{}' --preview-window=80%
+    pipenv run ls ./scripts/ | grep -Gv "[\_\.]py" | fzf --header="Select What You Want to Do" --header-border=bold --header-label-pos=top --no-multi --preview='pyfiglet -w$FZF_PREVIEW_COLUMNS -fansi_regular -jcenter MatStat && grep -G ^#[^!] ./scripts/{}' --preview-window=70%
 }
 
 selected=$(ScriptSelect)
 while [ "$selected" ]; do
     pipenv run python "./scripts/$selected"
+    read -p "Done with \`$selected\`. Press Enter to continue or Ctrl-c to exit... \
+        "
     selected=$(ScriptSelect)
 done
-
-echo "Done with utility"
